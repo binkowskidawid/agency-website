a1b5cf8acf28dfc9f0e8d65f0b72101619c12b34
Dawid Binkowski <d.k.binkowski@gmail.com>
2022-04-19 15:02:51 +0200

    initial commit with header

---
+++ components/Main.js
@@ -0,0 +1,8 @@
+export default function Main() {
+	return (
+		<main
+			className="l-header w-full min-h-full absolute top-full left-0 bg-top  bg-gradient-to-b from-[#764ba2] to-[#667eea]
+        scroll-smooth"
+		></main>
+	);
+}
---
+++ components/MobileMenu.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+	MenuIcon,
+} from "@heroicons/react/solid";
+
+export default function MobileMenu() {
+	return (
+		<div className="fixed">
+
+		</div>
+	);
+}
