de9097270e654b27bf409aba4858d8cb4ede487a
Dawid Binkowski <d.k.binkowski@gmail.com>
2022-05-04 12:13:08 +0200

    Changes in  Pages

--- pages/contact.js
+++ pages/contact.js
@@ -10,7 +10,7 @@ export default function PageContact() {
 				className="l-header w-full relative top-3 left-0 bg-top bg-gradient-to-b from-[#330867] to-[#774da4]
 			scroll-smooth"
 			>
-				<div className="h-screen flex flex-col md:flex-row items-center justify-around pt-20 md:pt-28 pb-10 px-6 md:px-16">
+				<div className="min-h-screen flex flex-col md:flex-row items-center justify-around pt-20 md:pt-28 pb-10 px-6 md:px-16">
 					<div className="flex flex-col flex-1 items-start justify-around text-white">
 						<h3 className="text-4xl md:text-5xl font-bold pb-4 font-['Antic_Didone']">
 							Contact
